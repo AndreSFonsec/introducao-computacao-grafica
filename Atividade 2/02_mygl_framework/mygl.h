0e12ff33069d8a0bc904bdb2820f64df99f25fa7
André Fonsêca <andre2013silva39@gmail.com>
2020-10-20 20:12:24 -0300

    atividade 2 da disciplina

---
+++ Atividade 2/02_mygl_framework/mygl.h
@@ -0,0 +1,14 @@
+#ifndef MYGL_H
+#define MYGL_H
+
+#include <string.h>
+
+#include "core.h"
+#include "frame_buffer.h"
+
+// Declaração da função que chamará as funções implementadas pelo aluno
+void MyGlDraw(void);
+void putPixel();
+void drawline();
+
+#endif  // MYGL_H
